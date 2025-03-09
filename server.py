46c0338c8ab9d004ded2124986de84e3215a66fb
BIOHac9D3 <122727208+BIOHac9D3@users.noreply.github.com>
2025-03-09 06:23:41 +0200

    Add server.py 1741494221096

---
+++ server.py
@@ -0,0 +1,16 @@
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+import os
+
+class CustomHandler(SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        # Set the directory to frontend
+        super().__init__(*args, directory="frontend", **kwargs)
+
+def run(server_class=HTTPServer, handler_class=CustomHandler, port=8000):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f"Server running at http://localhost:{port}/")
+    httpd.serve_forever()
+
+if __name__ == '__main__':
+    run()
