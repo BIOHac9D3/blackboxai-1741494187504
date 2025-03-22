49fca5c22e489f738b481e132067be34f1354aa1
BIOHac9D3 <122727208+BIOHac9D3@users.noreply.github.com>
2025-03-22 09:31:36 +0200

    Add CMSPython/tailwind.config.js 1742628696537

---
+++ CMSPython/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/templates/**/*.html",
+    "./app/static/js/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}
