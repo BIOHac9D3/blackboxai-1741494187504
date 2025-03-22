8381faa15e5d21a9ce66ec39aeb6845eaf308de8
BIOHac9D3 <122727208+BIOHac9D3@users.noreply.github.com>
2025-03-22 09:33:13 +0200

    Add app.py 1742628793364

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from backend.routes.register_routes import init_app
+
+app = Flask(__name__)
+
+# Initialize the application with all routes
+init_app(app)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
